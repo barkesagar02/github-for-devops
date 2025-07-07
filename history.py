4104c5c1145b00ec6971fb24d60e3e5dd9ecc631
barkesagar02 <barkesagar02@gmail.com>
2025-07-07 16:27:55 +0530

    Remove sensitive data

--- history.py
+++ history.py
@@ -1,8 +1,53 @@
-# dir
-# git status
-# rm .\testing.py
-# git status
-# dir
-# git restore
-# git restore .\testing.py
-# git status
+# PS H:\Study\Devops\github-for-devops> history     
+
+#   Id CommandLine
+#   -- -----------
+#    1 try { . "c:\Users\Nikhil Barke\AppData\Local\Programs\Microsoft VS Code\resources\app\out\vs\workbench\contrib\terminal\common\scripts\shellIntegration.ps1" } catch {}
+#    2 dir
+#    3 git status
+#    4 rm .\testing.py
+#    5 git status
+#    6 dir
+#    7 git restore
+#    8 git restore .\testing.py
+#    9 git status
+#   10 history
+#   11 dir
+#   12 git status
+#   13 git commit -m "stagged history file for testing" .\history.py
+#   14 git add .\testing.py
+#   15 git status
+#   16 git add .\history.py
+#   17 git status
+#   18 git restore --stagged .\history.py
+#   19 git restore --staged .\history.py
+#   20 git status
+#   21 git add .\history.py
+#   22 git status
+#   23 git commit -m "staged history file for testing" .\history.py
+#   24 git status
+#   25 git status
+#   26 dir
+#   28 git status
+#   29 dir
+#   30 git remote -v
+#   31 git remote -v
+#   33 git remote -v
+#   34 git branch -M main
+#   35 git push -u origin main
+#   37 git push -u origin main\
+#   38 git push -u origin main
+#   39 git branch -M main
+#   40 git push -u origin main
+#   41 git status
+#   42 git add .
+#   43 git status
+#   44 git commit -m "testing new file and modified file"
+#   45 dir
+#   46 git push -u origin main
+#   47 git status
+#   48 git push -u origin main
+#   49 git push
+#   50 git remote -v
+#   53 git remote -v
+#   54 git push
